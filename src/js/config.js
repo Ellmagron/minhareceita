e1b041699324c13ce4eda9261536430a620983f8
Ellmagron <shikabane.developer@gmail.com>
2023-08-28 13:34:21 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd03b954e-7c36-4cf0-9439-ba645b63a298';
+export const MODAL_CLOSE_SEC = 2.5;
