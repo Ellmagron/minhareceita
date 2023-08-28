3a19d140c2acde56e6588ae45682bb7dc265f673
Ellmagron <shikabane.developer@gmail.com>
2023-08-28 16:44:07 -0300

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -123,10 +123,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function(){
-  console.log('Welcome to aplication');
-}
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -135,7 +131,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature()
+  
 };
 
 init();
