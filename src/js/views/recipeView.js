48df1b4dc3fa521c083e89fb61c497347106de3b
Ellmagron <shikabane.developer@gmail.com>
2023-08-28 16:34:17 -0300

    fix bug

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import View from './View.js';
+import fracty from 'fracty';
 
 //import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg';
@@ -128,7 +129,8 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity) : ''
+          ing.quantity ? 
+          fracty(ing.quantity) : ''
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
